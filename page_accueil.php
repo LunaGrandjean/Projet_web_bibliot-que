783d2a100841f400e4af9606773bd8d6c1f6a1f8
thibautRV <125503955+thibautRV@users.noreply.github.com>
2023-12-20 14:54:40 +0100

    Update page_accueil.php

--- page_accueil.php
+++ page_accueil.php
@@ -1,144 +1,127 @@
 <!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title>
-            Test
-        </title>
-        <link rel="stylesheet" href="style.css"/>
-        <script type="text/javascript" src="page_accueil.js"></script>
-    </head>
-    <body>
-        <div id="recherche_livre">
-            <form action="page_accueil.php" method="post">
-            <?php
-                if (array_key_exists("livre_cherche", $_POST)) {
-                    echo '<div class="recherche"><input type="text" name="livre_cherche" placeholder="Chercher un livre..." value="' . $_POST["livre_cherche"] . '"/><button type="submit">Chercher</button></div>';
-                    $chercher_par_titre = $chercher_par_resume = $chercher_par_domaine = $chercher_par_auteur = false;
-                }
-                else {
-                    echo '<div class="recherche"><input type="text" name="livre_cherche" placeholder="Chercher un livre..."/><button type="submit">Chercher</button></div>';
-                    $chercher_par_titre = $chercher_par_resume = $chercher_par_domaine = $chercher_par_auteur = true;
-                }
-                $titre_checked = $resume_checked = $domaine_checked = $auteur_checked = "";
-                if (array_key_exists("chercher_par_titre", $_POST)) {
-                    $chercher_par_titre = true;
-                }
-                if (array_key_exists("chercher_par_resume", $_POST)) {
-                    $chercher_par_resume = true;
-                }
-                if (array_key_exists("chercher_par_domaine", $_POST)) {
-                    $chercher_par_domaine = true;
-                }
-                if (array_key_exists("chercher_par_auteur", $_POST)) {
-                    $chercher_par_auteur = true;
-                }
-                if ($chercher_par_titre) {
-                    $titre_checked = 'checked="checked"';
-                }
-                if ($chercher_par_resume) {
-                    $resume_checked = 'checked="checked"';
-                }
-                if ($chercher_par_domaine) {
-                    $domaine_checked = 'checked="checked"';
-                }
-                if ($chercher_par_auteur) {
-                    $auteur_checked = 'checked="checked"';
-                }
-                echo '<br><br>
-                Chercher par :
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <label class="switch">
-                <input type="checkbox" name="chercher_par_titre"' . $titre_checked .'>
-                <span class="slider"></span>
-                </label>
-                <label for="chercher_par_titre">Titre</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <label class="switch">
-                <input type="checkbox" name="chercher_par_resume"' . $resume_checked . '>
-                <span class="slider"></span>
-                </label>
-                <label for="chercher_par_resume">Résumé</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <label class="switch">
-                <input type="checkbox"  name="chercher_par_domaine"' . $domaine_checked . '>
-                <span class="slider"></span>
-                </label>
-                <label for="chercher_par_domaine">Domaine</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <label class="switch">
-                <input type="checkbox" name="chercher_par_auteur"' . $auteur_checked . '>
-                <span class="slider"></span>
-                </label>
-                <label for="chercher_par_auteur">Auteur</label>';
-                echo '<div id="conteneur_filtres">
-                <span id="afficher_filtres" onclick="afficher_filtres();">&#9654; Filtres</span>
-                <div id="filtres"></div>
-                </div>';
-            ?>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Test</title>
+    <link rel="stylesheet" href="styles.css" media="screen" type="text/css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div id="recherche_livre">
+        <form action="page_acceuil.php" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Rechercher un livre..." name="search" aria-label="Rechercher un livre" aria-describedby="button-addon2" value="<?php echo isset($_POST['search']) ? htmlspecialchars($_POST['search']) : ''; ?>">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Rechercher</button>
+                </div>
+            
             <br>
             <br>
-            <div id="resultats_livres">
-                <?php
-                if (array_key_exists("livre_cherche", $_POST)) {
-                    $livre_cherche = $_POST["livre_cherche"];
-                    $conn = mysqli_connect("localhost", "root", "", "projetweb");
-                    $requete = 'SELECT DISTINCT livre.* FROM livre
-                    INNER JOIN ecrit ON ecrit.Livre_ISSN = livre.ISSN
-                    INNER JOIN auteur ON auteur.Num = ecrit.Auteur_Num
-                    WHERE ';
-                    $chercher_par = '';
-                    if ($chercher_par_titre) {
-                        $chercher_par = $chercher_par . 'Titre, " ",';
-                    }
-                    if ($chercher_par_resume) {
-                        $chercher_par = $chercher_par . 'Resume, " ",';
-                    }
-                    if ($chercher_par_domaine) {
-                        $chercher_par = $chercher_par . 'Domaine, " ",';
-                    }
-                    if ($chercher_par_auteur) {
-                        $chercher_par = $chercher_par . 'auteur.Nom, " ", auteur.Prenom,';
-                    }
-                    if ($chercher_par=="") {
-                        echo '<div class="alert">
-                        <span class="closebtn" onclick="this.parentElement.style.display=' . "'none'" . ';">&times;</span>
-                        Il faut chercher au moins par titre, résumé, domaine ou auteur.
-                        </div>';
-                    }
-                    else {
-                        //Il devrait y avoir une virgule à la fin de $chercher_par, donc on la retire
-                        $chercher_par = substr($chercher_par, 0, -1);
-                        foreach (explode(" ", $livre_cherche) as $terme) {
-                            $requete = $requete . ' CONCAT('. $chercher_par .')
-                            LIKE "%' . substr($terme, 0, 5) . '%" OR ';
-                        };
-                        $requete = substr($requete, 0, -4);
-                        $res = mysqli_query($conn, $requete);
-                        $books = [];
-                        while ($row = mysqli_fetch_assoc($res)) {
-                            $books[] = $row;
-                        };
-                        echo "<table>
-                        <tr>
-                        <td>ISSN</td>
-                        <td>Titre</td>
-                        <td>Résumé</td>
-                        <td>Nombre de pages</td>
-                        <td>Domaine</td>
-                        </tr>";
-                        foreach ($books as $book) {
-                            echo "<tr>";
-                            foreach ($book as $value) {
-                                echo "<td>$value</td>";
-                            }
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                    }
-                }
-                ?>
-            </div>
-            </form>
+            Chercher par :
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label class="switch">
+                <input type="checkbox" name="chercher_par_titre" <?php echo isset($_POST["chercher_par_titre"]) ? 'checked="checked"' : ''; ?>>
+                <span class="slider"></span>
+            </label>
+            <label for="chercher_par_titre">Titre</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label class="switch">
+                <input type="checkbox" name="chercher_par_resume" <?php echo isset($_POST["chercher_par_resume"]) ? 'checked="checked"' : ''; ?>>
+                <span class="slider"></span>
+            </label>
+            <label for="chercher_par_resume">Résumé</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label class="switch">
+                <input type="checkbox" name="chercher_par_domaine" <?php echo isset($_POST["chercher_par_domaine"]) ? 'checked="checked"' : ''; ?>>
+                <span class="slider"></span>
+            </label>
+            <label for="chercher_par_domaine">Domaine</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <label class="switch">
+                <input type="checkbox" name="chercher_par_auteur" <?php echo isset($_POST["chercher_par_auteur"]) ? 'checked="checked"' : ''; ?>>
+                <span class="slider"></span>
+            </label>
+            <label for="chercher_par_auteur">Auteur</label>
+        </form>
+
+        <div id="resultats_livres">
+        <?php
+            $host = 'localhost';
+            $db_name = 'projetweb';
+            $username = 'root';
+            $password = '';
+
+            try {
+                $dbh = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die('Échec de la connexion à la base de données : ' . $e->getMessage());
+            }
+
+            // Requête SQL de base
+            $sql_select = "SELECT livre.ISSN, Titre, Resume, Nbpages, Domaine, GROUP_CONCAT(CONCAT(Nom, ' ', Prenom) SEPARATOR ', ') AS Auteurs
+            FROM livre
+            LEFT JOIN Ecrit ON livre.ISSN = Ecrit.Livre_ISSN
+            LEFT JOIN Auteur ON Ecrit.Auteur_Num = Auteur.Num
+            WHERE Titre LIKE :search
+                OR Nbpages LIKE :search
+                OR Domaine LIKE :search
+                OR CONCAT(Nom, ' ', Prenom) LIKE :search
+            GROUP BY livre.ISSN";
+
+            // Si une recherche est effectuée
+            if (isset($_POST['search'])) {
+                $search = $_POST['search'];
+                // Modifiez la requête pour inclure des conditions de recherche
+                $sql_select = "SELECT livre.ISSN, Titre, Resume, Nbpages, Domaine, GROUP_CONCAT(CONCAT(Nom, ' ', Prenom) SEPARATOR ', ') AS Auteurs
+                    FROM livre
+                    LEFT JOIN Ecrit ON livre.ISSN = Ecrit.Livre_ISSN
+                    LEFT JOIN Auteur ON Ecrit.Auteur_Num = Auteur.Num
+                    WHERE Titre LIKE :search
+                        OR Nbpages LIKE :search
+                        OR Domaine LIKE :search
+                        OR CONCAT(Nom, ' ', Prenom) LIKE :search
+                    GROUP BY livre.ISSN";
+            }
+
+            $stmt_select = $dbh->prepare($sql_select);
+
+            echo "<form action='supprimer_livres.php' method='post'>";
+            echo "<table class='table'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th scope='col'>ISSN</th>";
+            echo "<th scope='col'>Titre</th>";
+            echo "<th scope='col'>Resume</th>";
+            echo "<th scope='col'>Nbpages</th>";
+            echo "<th scope='col'>Domaine</th>";
+            echo "<th scope='col'>Auteur</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+
+            // Bind the parameter only once
+            $search = '%' . $_POST['search'] . '%';
+            $stmt_select->bindParam(':search', $search, PDO::PARAM_STR);
+            $stmt_select->execute();
+
+            while ($row = $stmt_select->fetch(PDO::FETCH_ASSOC)) {
+                echo "<tr>";
+                echo "<th scope='row'>{$row['ISSN']}</th>";
+                echo "<td>{$row['Titre']}</td>";
+                echo "<td>{$row['Resume']}</td>";
+                echo "<td>{$row['Nbpages']} pages</td>";
+                echo "<td>{$row['Domaine']}</td>";
+                echo "<td>{$row['Auteurs']}</td>";
+                echo "</tr>";
+            }
+
+            echo "</tbody>";
+            echo "</table>";
+            echo "</form>";
+
+            $dbh = null;
+        ?>
         </div>
-        <div id="graphiques">
-        </div>
-    </body>
+    </div>
+</body>
 </html>
