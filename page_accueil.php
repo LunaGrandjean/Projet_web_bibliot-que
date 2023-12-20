acb758273e439481d1d3979a1d42d6b46db49ea8
lapin2048 <lapin2048@gmail.com>
2023-12-20 11:20:46 +0100

    Ajout de fonctionnalités pour la recherche de livres
    J'ai ajouté la possibilité de chercher par titre, résumé, domaine ou auteur (plutôt que par tous les quatre à la fois).

--- page_accueil.php
+++ page_accueil.php
@@ -13,10 +13,60 @@
             <?php
                 if (array_key_exists("livre_cherche", $_POST)) {
                     echo '<div class="recherche"><input type="text" name="livre_cherche" placeholder="Chercher un livre..." value="' . $_POST["livre_cherche"] . '"/><button type="submit">Chercher</button></div>';
+                    $chercher_par_titre = $chercher_par_resume = $chercher_par_domaine = $chercher_par_auteur = false;
                 }
                 else {
                     echo '<div class="recherche"><input type="text" name="livre_cherche" placeholder="Chercher un livre..."/><button type="submit">Chercher</button></div>';
+                    $chercher_par_titre = $chercher_par_resume = $chercher_par_domaine = $chercher_par_auteur = true;
                 }
+                $titre_checked = $resume_checked = $domaine_checked = $auteur_checked = "";
+                if (array_key_exists("chercher_par_titre", $_POST)) {
+                    $chercher_par_titre = true;
+                }
+                if (array_key_exists("chercher_par_resume", $_POST)) {
+                    $chercher_par_resume = true;
+                }
+                if (array_key_exists("chercher_par_domaine", $_POST)) {
+                    $chercher_par_domaine = true;
+                }
+                if (array_key_exists("chercher_par_auteur", $_POST)) {
+                    $chercher_par_auteur = true;
+                }
+                if ($chercher_par_titre) {
+                    $titre_checked = 'checked="checked"';
+                }
+                if ($chercher_par_resume) {
+                    $resume_checked = 'checked="checked"';
+                }
+                if ($chercher_par_domaine) {
+                    $domaine_checked = 'checked="checked"';
+                }
+                if ($chercher_par_auteur) {
+                    $auteur_checked = 'checked="checked"';
+                }
+                echo '<br><br>
+                Chercher par :
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <label class="switch">
+                <input type="checkbox" name="chercher_par_titre"' . $titre_checked .'>
+                <span class="slider"></span>
+                </label>
+                <label for="chercher_par_titre">Titre</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <label class="switch">
+                <input type="checkbox" name="chercher_par_resume"' . $resume_checked . '>
+                <span class="slider"></span>
+                </label>
+                <label for="chercher_par_resume">Résumé</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <label class="switch">
+                <input type="checkbox"  name="chercher_par_domaine"' . $domaine_checked . '>
+                <span class="slider"></span>
+                </label>
+                <label for="chercher_par_domaine">Domaine</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <label class="switch">
+                <input type="checkbox" name="chercher_par_auteur"' . $auteur_checked . '>
+                <span class="slider"></span>
+                </label>
+                <label for="chercher_par_auteur">Auteur</label>'
             ?>
             <br>
             <br>
@@ -29,32 +79,55 @@
                     INNER JOIN ecrit ON ecrit.Livre_ISSN = livre.ISSN
                     INNER JOIN auteur ON auteur.Num = ecrit.Auteur_Num
                     WHERE ';
-                    foreach (explode(" ", $livre_cherche) as $terme) {
-                        $requete = $requete . ' CONCAT(Titre, " ", Resume, " ", Domaine, " ", auteur.Nom, " ", auteur.Prenom)
-                        LIKE "%' . substr($terme, 0, 5) . '%" OR ';
-                    };
-                    $requete = substr($requete, 0, -4);
-                    $res = mysqli_query($conn, $requete);
-                    $books = [];
-                    while ($row = mysqli_fetch_assoc($res)) {
-                        $books[] = $row;
-                    };
-                    echo "<table>
-                    <tr>
-                    <td>ISSN</td>
-                    <td>Titre</td>
-                    <td>Résumé</td>
-                    <td>Nombre de pages</td>
-                    <td>Domaine</td>
-                    </tr>";
-                    foreach ($books as $book) {
-                        echo "<tr>";
-                        foreach ($book as $value) {
-                            echo "<td>$value</td>";
-                        }
-                        echo "</tr>";
+                    $chercher_par = '';
+                    if ($chercher_par_titre) {
+                        $chercher_par = $chercher_par . 'Titre, " ",';
                     }
-                    echo "</table>";
+                    if ($chercher_par_resume) {
+                        $chercher_par = $chercher_par . 'Resume, " ",';
+                    }
+                    if ($chercher_par_domaine) {
+                        $chercher_par = $chercher_par . 'Domaine, " ",';
+                    }
+                    if ($chercher_par_auteur) {
+                        $chercher_par = $chercher_par . 'auteur.Nom, " ", auteur.Prenom,';
+                    }
+                    if ($chercher_par=="") {
+                        echo '<div class="alert">
+                        <span class="closebtn" onclick="this.parentElement.style.display=' . "'none'" . ';">&times;</span>
+                        Il faut chercher au moins par titre, résumé, domaine ou auteur.
+                        </div>';
+                    }
+                    else {
+                        //Il devrait y avoir une virgule à la fin de $chercher_par, donc on la retire
+                        $chercher_par = substr($chercher_par, 0, -1);
+                        foreach (explode(" ", $livre_cherche) as $terme) {
+                            $requete = $requete . ' CONCAT('. $chercher_par .')
+                            LIKE "%' . substr($terme, 0, 5) . '%" OR ';
+                        };
+                        $requete = substr($requete, 0, -4);
+                        $res = mysqli_query($conn, $requete);
+                        $books = [];
+                        while ($row = mysqli_fetch_assoc($res)) {
+                            $books[] = $row;
+                        };
+                        echo "<table>
+                        <tr>
+                        <td>ISSN</td>
+                        <td>Titre</td>
+                        <td>Résumé</td>
+                        <td>Nombre de pages</td>
+                        <td>Domaine</td>
+                        </tr>";
+                        foreach ($books as $book) {
+                            echo "<tr>";
+                            foreach ($book as $value) {
+                                echo "<td>$value</td>";
+                            }
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
                 }
                 ?>
             </div>
