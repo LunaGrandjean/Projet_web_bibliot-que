d7ac4e0005ac123878f6fe92a293f393fb683524
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-12-20 10:39:31 +0100

    Update page_livres.php

--- page_livres.php
+++ page_livres.php
@@ -33,7 +33,6 @@
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" href="#" data-bs-toggle="offcanvas" data-bs-target="#profileMenu" aria-controls="profileMenu">
-            <!-- Utilisez l'icône SVG ici -->
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
               <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
@@ -45,7 +44,6 @@
   </div>
 </nav>
 
-<!-- Barre latérale (offcanvas) -->
 <div class="offcanvas offcanvas-end" tabindex="-1" id="profileMenu" aria-labelledby="profileMenuLabel">
   <div class="offcanvas-header">
     <h5 class="offcanvas-title" id="profileMenuLabel">Menu Profil</h5>
