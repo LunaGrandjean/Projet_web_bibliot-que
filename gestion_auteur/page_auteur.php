f39583999c211f0c9a8aa26c50b42371696334cc
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-12-21 10:16:17 +0100

    Update page_auteur.php

--- gestion_auteur/page_auteur.php
+++ gestion_auteur/page_auteur.php
@@ -33,6 +33,7 @@
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" href="#" data-bs-toggle="offcanvas" data-bs-target="#profileMenu" aria-controls="profileMenu">
+            <!-- Utilisez l'icône SVG ici -->
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
               <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
@@ -43,6 +44,37 @@
     </div>
   </div>
 </nav>
+
+<!-- Barre latérale (offcanvas) -->
+<div class="offcanvas offcanvas-end" tabindex="-1" id="profileMenu" aria-labelledby="profileMenuLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="profileMenuLabel">Menu Profil</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class="list-group">
+    <li class="list-group-item">
+  <a href="settings.php" class="text-decoration-none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+      <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+    </svg>
+    Paramètres
+  </a>
+</li>
+
+<li class="list-group-item">
+  <a href="deconnexion.php" class="text-decoration-none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+      <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+    </svg>
+    Déconnexion
+  </a>
+</li>
+
+    </ul>
+  </div>
+</div>
 
 <div class="container mt-4">
   <h2>Liste des auteurs</h2>
@@ -65,12 +97,30 @@
   $sql_select = "SELECT * FROM auteur";
   $stmt_select = $dbh->query($sql_select);
 
-  echo "<ul>";
+  echo "<table class='table'>";
+  echo "<thead>";
+  echo "<tr>";
+  echo "<th scope='col'>Nom</th>";
+  echo "<th scope='col'>Prénom</th>";
+  echo "<th scope='col'>Date de Naissance</th>";
+  echo "<th scope='col'>Nationalité</th>";
+  echo "<th scope='col'>Action</th>";
+  echo "</tr>";
+  echo "</thead>";
+  echo "<tbody>";
+
   while ($row = $stmt_select->fetch(PDO::FETCH_ASSOC)) {
-    echo "<li>{$row['Nom']} - {$row['Prenom']} - {$row['DateNaissance']} - {$row['Nationalite']} ";
-    echo "<a href='supprimer_auteur.php?Num={$row['Num']}' class='btn btn-link btn-sm' onclick='return confirm(\"Êtes-vous sûr de vouloir supprimer cet auteur ?\")'>Supprimer</a></li>";
+    echo "<tr>";
+    echo "<td>{$row['Nom']}</td>";
+    echo "<td>{$row['Prenom']}</td>";
+    echo "<td>{$row['DateNaissance']}</td>";
+    echo "<td>{$row['Nationalite']}</td>";
+    echo "<td><a href='supprimer_auteur.php?Num={$row['Num']}' class='btn btn-link btn-sm' onclick='return confirm(\"Êtes-vous sûr de vouloir supprimer cet auteur ?\")'>Supprimer</a></td>";
+    echo "</tr>";
   }
-  echo "</ul>";
+
+  echo "</tbody>";
+  echo "</table>";
   ?>
 </div>
 
