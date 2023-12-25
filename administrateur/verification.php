e3d3441253e47098f9576e0bc14205e13fe1dd2e
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-12-25 11:09:30 +0100

    Add files via upload

---
+++ administrateur/verification.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include('db.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM administrateur WHERE Nom = :username AND password = :password";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    $stmt->execute();
+
+    if ($user = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $_SESSION['username'] = $user['username'];
+        header('Location: page_admin.php');
+        exit();
+    } else {
+        $_SESSION['error_message'] = 'Identifiants incorrects';
+        header('Location: connexion_admin.php');
+        exit();
+    }
+}
+?>
