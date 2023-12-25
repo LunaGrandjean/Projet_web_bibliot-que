76f1520c2d9e4f3de654207e014063e6878159d2
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-12-25 11:08:57 +0100

    Delete deconnexion.php

--- deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Détruisez la session et redirigez vers la page d'accueil
-session_start();
-session_destroy();
-header('Location: http://localhost/Projet_web_bibliotheque/page_acceuil/index.php');
-exit();
-?>
