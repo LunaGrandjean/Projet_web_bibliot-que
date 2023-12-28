cd85e2ba3f0809a1e1f272e99fd27fb10343816e
lapin2048 <lapin2048@gmail.com>
2023-12-28 16:09:58 +0100

    include("../db.php"); dans "domaines_livres.php"

--- page_accueil/domaines_livres.php
+++ page_accueil/domaines_livres.php
@@ -1,15 +1,6 @@
 <?php
-    $host = 'localhost';
-    $db_name = 'projetweb';
-    $username = 'root';
-    $password = '';
-
-    try {
-        $dbh = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-    }
-    $stmt_select = $dbh->prepare("SELECT DISTINCT Domaine FROM livre");
+    include("../db.php");
+    $stmt_select = $pdo->prepare("SELECT DISTINCT Domaine FROM livre");
     $stmt_select->execute();
     while ($row = $stmt_select->fetch(PDO::FETCH_ASSOC)) {
         echo "{$row['Domaine']};";
