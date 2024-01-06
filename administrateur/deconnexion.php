dbf8aefa5fe39191ea07346419eebc6a063dac86
maiajouenne <125506491+maiajouenne@users.noreply.github.com>
2024-01-06 18:34:29 +0100

    Update deconnexion.php

--- administrateur/deconnexion.php
+++ administrateur/deconnexion.php
@@ -2,6 +2,6 @@
 // Détruire la session et redirigez vers la page d'accueil
 session_start();
 session_destroy();
-header('Location: index.php');
+header('Location: ../page_accueil/index.php');
 exit();
 ?>
